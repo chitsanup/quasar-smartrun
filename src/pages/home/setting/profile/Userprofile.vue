50087c97951fdcdfec1b5b571f3eeeb10f3be42a
pattara <hammee196@gmail.com>
2019-09-29 22:05:09 +0700

    update again

--- src/pages/home/setting/profile/Userprofile.vue
+++ src/pages/home/setting/profile/Userprofile.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import userprofile from "../../../../components/Page/Users/Userprofile"
+import userprofile from "../../../../components/Page/Users/UserProfile"
 import pagecover from "../../../../components/Page/Users/PageCover"
 import logout from "../../../../components/Page/Users/LogoutButton"
 export default {
